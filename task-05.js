8ca5d6c0e24520f3323f2e0b46c393748acb6acb
Vito <topvetal@gmail.com>
2021-02-07 14:59:02 +0200

    task-05

---
+++ task-05.js
@@ -0,0 +1,8 @@
+// Задание 5
+// Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input),
+// подставляет его текущее значение в span#name-output.
+// Если инпут пустой, в спане должна отображаться строка 'незнакомец'.
+
+let input = document.getElementById('name-input')
+let output = document.getElementById('name-output')
+input.addEventListener('keyup', () => output.innerHTML = input.value)
